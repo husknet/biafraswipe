4ed7c9a1b9fbed35befb70d616f5ff13e5d5f6ac
husknet <127146284+husknet@users.noreply.github.com>
2024-06-28 20:55:06 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { Web3ModalProvider } from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ModalProvider>
+      <App />
+    </Web3ModalProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+reportWebVitals();
